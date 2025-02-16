bb501ffc5641553d17e3d70f967f6ca670731110
farhadahmed1 <farhad.ahmed.aust@gmail.com>
2025-02-16 18:27:32 +0600

    academic semester add

--- src/components/form/PHForm.tsx
+++ src/components/form/PHForm.tsx
@@ -1,3 +1,4 @@
+import { Form } from "antd";
 import { ReactNode } from "react";
 import {
   FieldValues,
@@ -5,15 +6,37 @@ import {
   SubmitHandler,
   useForm,
 } from "react-hook-form";
+type TFormConfig = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  defaultValues?: Record<string, any>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  resolver?: any;
+};
 type TFormProps = {
   onSubmit: SubmitHandler<FieldValues>;
   children: ReactNode;
-};
-const PHForm = ({ onSubmit, children }: TFormProps) => {
-  const methods = useForm();
+} & TFormConfig;
+const PHForm = ({
+  onSubmit,
+  children,
+  defaultValues,
+  resolver,
+}: TFormProps) => {
+  const formConfig: TFormConfig = {};
+
+  if (defaultValues) {
+    formConfig["defaultValues"] = defaultValues;
+  }
+
+  if (resolver) {
+    formConfig["resolver"] = resolver;
+  }
+  const methods = useForm(formConfig);
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+      <Form layout="vertical" onFinish={methods.handleSubmit(onSubmit)}>
+        {children}
+      </Form>
     </FormProvider>
   );
 };
--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,19 +1,61 @@
 import { Button, Col, Flex } from "antd";
 import PHForm from "./../../../components/form/PHForm";
-import PHInput from "./../../../components/form/PHInput";
 import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHSelect from "../../../components/form/PHSelect";
+import { monthOptions } from "../../../constants/global";
+import { semesterOptions } from "../../../constants/semester";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
+import { useAddAcademicSemestersMutation } from "../../../redux/features/admin/academicManagement.api";
+import { toast } from "sonner";
+const nameOptions = [
+  { value: "01", label: "Autumn" },
+  { value: "02", label: "Summer" },
+  { value: "03", label: "Fall" },
+];
 
+const currentYear = new Date().getFullYear();
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+  value: String(currentYear + number),
+  label: String(currentYear + number),
+}));
 const CreateAcademicSemester = () => {
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+  const [addAcademicSemester] = useAddAcademicSemestersMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const name = nameOptions[Number(data?.name) - 1]?.label;
+    const semesterData = {
+      name,
+      code: data.name,
+      year: data.year,
+      startMonth: data.startMonth,
+      endMonth: data.endMonth,
+    };
+    try {
+      console.log(semesterData);
+      const res = await addAcademicSemester(semesterData);
+      //console.log(res);
+      toast.success(res.data.message);
+    } catch (err) {
+      toast.error("Something went wrong");
+    }
   };
 
   return (
-    <Flex justify="center">
+    <Flex justify="center" align="center">
       <Col span={6}>
-        <PHForm onSubmit={onSubmit}>
-          <PHInput type="text" name="name" label="Name" />
-          <PHInput type="text" name="year" label="Year" />
+        <PHForm
+          onSubmit={onSubmit}
+          resolver={zodResolver(academicSemesterSchema)}
+        >
+          <PHSelect label="Name" name="name" options={semesterOptions} />
+          <PHSelect label="Year" name="year" options={yearOptions} />
+          <PHSelect
+            label="Start Month"
+            name="startMonth"
+            options={monthOptions}
+          />
+          <PHSelect label="End Month" name="endMonth" options={monthOptions} />
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
